97231224851d6f079d0a6aadcabb1fce46e4da84
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-11-22 15:30:04 -0300

    Pure string replaced by dictionary

--- app/jai.py
+++ app/jai.py
@@ -1,12 +1,22 @@
-body_string = """
-{
+from random import random
+import math
+import json
+
+def generate_hexadecimal():
+    string_to_return = ""
+    charactersHexa = "0123456789abcdef"
+    for i in range(1,32):
+        string_to_return += charactersHexa[math.ceil(random() * 15)]
+    return string_to_return
+
+dictionary = {
     "_type": "export",
     "__export_format": 4,
     "__export_date": "2024-11-22T15:52:03.019Z",
     "__export_source": "insomnia.desktop.app:v2023.5.8",
     "resources": [
         {
-            "_id": "req_38c2040e0731415cbd78f37a562eb4fe",
+            "_id": "req_a7f2d3e8b4c5a1d6e9f3b2c7d8a0e1f",
             "parentId": "wrk_ed31ef85796444faafcdc83f75440355",
             "modified": 1732290693730,
             "created": 1732290685432,
@@ -24,18 +34,46 @@ body_string = """
             ],
             "authentication": {},
             "metaSortKey": -1732290685432,
-            "isPrivate": false,
-            "settingStoreCookies": true,
-            "settingSendCookies": true,
-            "settingDisableRenderRequestBody": false,
-            "settingEncodeUrl": true,
-            "settingRebuildPath": true,
+            "isPrivate": False,
+            "settingStoreCookies": True,
+            "settingSendCookies": True,
+            "settingDisableRenderRequestBody": False,
+            "settingEncodeUrl": True,
+            "settingRebuildPath": True,
+            "settingFollowRedirects": "global",
+            "_type": "request"
+        },
+        {
+            "_id": "req_c8f1a4e7b2d6f3a9d0e5b3c7a8f2d1",
+            "parentId": "wrk_ed31ef85796444faafcdc83f75440355",
+            "modified": 1732290693730,
+            "created": 1732290685432,
+            "url": "http://localhost",
+            "name": "New Request 2",
+            "description": "",
+            "method": "GET",
+            "body": {},
+            "parameters": [],
+            "headers": [
+                {
+                    "name": "User-Agent",
+                    "value": "insomnia/2023.5.8"
+                }
+            ],
+            "authentication": {},
+            "metaSortKey": -1732290685432,
+            "isPrivate": False,
+            "settingStoreCookies": True,
+            "settingSendCookies": True,
+            "settingDisableRenderRequestBody": False,
+            "settingEncodeUrl": True,
+            "settingRebuildPath": True,
             "settingFollowRedirects": "global",
             "_type": "request"
         },
         {
             "_id": "wrk_ed31ef85796444faafcdc83f75440355",
-            "parentId": null,
+            "parentId": None,
             "modified": 1732290683084,
             "created": 1732290683084,
             "name": "Simple Model",
@@ -50,9 +88,9 @@ body_string = """
             "created": 1732290683086,
             "name": "Base Environment",
             "data": {},
-            "dataPropertyOrder": null,
-            "color": null,
-            "isPrivate": false,
+            "dataPropertyOrder": None,
+            "color": None,
+            "isPrivate": False,
             "metaSortKey": 1732290683086,
             "_type": "environment"
         },
@@ -67,6 +105,10 @@ body_string = """
         }
     ]
 }
-"""
 
-print(body_string)
+pre_result_string = json.dumps(dictionary, indent=4)
+pre_result_string = pre_result_string.replace("true", "True")
+pre_result_string = pre_result_string.replace("false", "False")
+
+
+print(pre_result_string)
